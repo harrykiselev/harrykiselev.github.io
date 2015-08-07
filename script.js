3cb74224fba5620bb5cab00c583243d6badaafc9
Harry Kiselev <harry.kiselev@gmail.com>
2015-08-08 01:42:52 +0300

    Add language choosing feature.

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-66099824-1', 'auto');
 ga('send', 'pageview');
+
+window.onload = function(){
+  var changeLanguage = function(e) {
+    choosenLanguage = this.getAttribute('data-language')
+    introSections = document.getElementsByClassName('intro')
+    for (var i = 0; i < introSections.length; i++) {
+      var section = introSections[i];
+      section.style.display = 'none'
+    }
+    visibleBlock = document.getElementById(choosenLanguage);
+    visibleBlock.style.display = 'inline-block';
+    e.preventDefault();
+  }
+
+  var languageItems = document.getElementsByClassName('intro__language-pane__item__link')
+  for (var i = 0; i < languageItems.length; i++) {
+    var item = languageItems[i];
+    item.addEventListener('click', changeLanguage, false);
+  }
+}
