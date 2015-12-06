df32850108d95acf641e004d9bbf539887f12e6f
Harry Kiselev <harry.kiselev@gmail.com>
2015-12-07 01:43:18 +0300

    On load page detecting user language and show required page.

--- script.js
+++ script.js
@@ -11,25 +11,43 @@ ga('send', 'pageview');
 // код страницы
 window.onload = function(){
 
-  var changeLanguage = function(e) {
-    // получаем язык, который нужен пользователю
-    choosenLanguage = this.getAttribute('data-language')
-    // скрываем все секции информации
-    introSections = document.getElementsByClassName('intro')
-    for (var i = 0; i < introSections.length; i++) {
-      var section = introSections[i];
-      section.style.display = 'none'
-    }
+  // функция изменения языка на сайте
+  var changeLanguage = function(choosenLanguage) {
+    // скрываем ненужную секцию информации
+    var closingTabLanguage = (choosenLanguage == 'ru') ? 'en' : 'ru';
+    document.getElementById(closingTabLanguage).style.display = 'none';
     // показываем нужную секцию информации
     document.getElementById(choosenLanguage).style.display = 'inline-block';
-    e.preventDefault();
+  }
+
+  // открываем таб с нужным языком
+  // проверяем есть ли что нибудь в хэшэ адреса (после решетки)
+  // http://harrykiselev.github.io/index.html#en
+  if (window.location.hash) {
+    var languageName = window.location.hash.substr(1)
+    changeLanguage(languageName)
+  } else {
+    // сюда попадаем, если нет ничего после решетки
+    // начинаем проверять доступные языки в браузере
+    // если в доступных языках есть русский - то отображаем соответствующий таб
+    if ( navigator.languages.indexOf('ru') >= 0 ) {
+      // включаем режим для русскоязычных людей
+      changeLanguage('ru')
+    } else {
+      // включаем режим для всех остальных землян
+      changeLanguage('en')
+    }
   }
 
   // обрабатываем панель переключения языков
   var languageItems = document.getElementsByClassName('intro__language-pane__item__link')
   for (var i = 0; i < languageItems.length; i++) {
     var item = languageItems[i];
-    item.addEventListener('click', changeLanguage, false);
+    item.addEventListener( 'click', function() {
+      // получаем язык, который нужен пользователю
+      var choosenLanguage = this.getAttribute('data-language');
+      changeLanguage(choosenLanguage)
+    }, false);
   }
 
 }
